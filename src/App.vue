1762eda13235dd719b6e2b7337959f1d7b23708a
Mengyue Zhang <mengyue.z@outlook.com>
2022-12-08 21:31:44 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import Nav from './components/Nav.vue'
+
+export default {
+  components :{
+    Nav,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <Nav />
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style>
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class = "nav-bar">
+        <a href="index.html">About Me</a>
+        <a href="carrot.html">About Carrot</a>
+        <a href="gallery.html">My Gallery</a>
+        <a href="portfolio.html">Portfolio</a>
+        <a href="contact.html">Contact</a>
+    </nav>
+</template>
+
+<style>
+.nav-bar{
+    margin: 0px;
+    overflow: hidden;
+    background-color: #BBBFCA;
+}
+
+.nav-bar a {
+    float: left;
+    display: block;
+    color: #fff;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+  
+.nav-bar a:hover {
+    background-color: #ddd;
+    color: black;
+}
+  
+.nav-bar a.active {
+    background-color: #ddd;
+    color: white;
+}
+</style>
